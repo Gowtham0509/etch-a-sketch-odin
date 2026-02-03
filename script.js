95834d174e5e966481571ece54a99faa706c65ed
Gowtham0509 <gowthamanandaraj02@gmail.com>
2026-02-03 17:06:39 +0530

    implemented grids

--- script.js
+++ script.js
@@ -1,9 +1,34 @@
-
-
 const container = document.querySelector(".container");
+const header = document.querySelector(".user-input");
 
-for(let i = 1; i <= 16 * 16; i++){
-        const gridItem = document.createElement("div");
-        gridItem.classList.add(`grid-item`);
-        container.appendChild(gridItem);
+function initialGrid(){
+    container.style.width = `${16 * 22}px`
+    for(let i = 1; i <= 16 * 16; i++){
+            const gridItem = document.createElement("div");
+            gridItem.classList.add(`grid-item`);
+            container.appendChild(gridItem);
+    }
 }
+
+
+
+function newUserGrid(userInput){
+    container.innerHTML = "";
+    container.style.width = `${userInput * 22}px`;  
+    for(let i = 1; i <= userInput * userInput; i++){
+            const gridItem = document.createElement("div");
+            gridItem.classList.add(`grid-item`);
+            container.appendChild(gridItem);
+        }
+}
+
+header.addEventListener("click", () => {
+    let userInput = Number(prompt("Enter the Grid size (Max 100): "));
+    if(!Number.isInteger(userInput) || userInput <= 0 || userInput > 100){
+        alert("Enter valid grid size between 1 to 100");
+        return;
+    }
+    newUserGrid(userInput);
+});
+
+initialGrid();
